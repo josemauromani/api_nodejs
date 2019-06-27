d4e1bc588052cb4b5717feaec17c8b49a3ad1440
Jose Mauro <developer@v8system.com>
2019-06-27 15:33:20 -0300

    git

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const urlDb = 'mongodb+srv://root:Ebenezer123@clusterapi-gseal.mongodb.net/test?retryWrites=true&w=majority';
+const optionsDb = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
+
+mongoose.connect(urlDb, optionsDb);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', (err) => {
+    console.log('Erro:' + err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Aplicação desconectada do banco');
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Aplicação conectada ao Banco de Dados');
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const indexRoutes = require('./routes/index');
 const usersRoutes = require('./routes/users');
--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,16 @@ router.get('/', (req, res) => {
     return res.send({ message: 'ok get users' });
 });
 
+router.get('/fotos', (req, res) => {
+    return res.send({ message: 'fotos do usuario' });
+});
+
 router.post('/', (req, res) => {
     return res.send({ message: 'ok post users' });
 });
+
+router.post('/create', (req, res) => {
+    return res.send({message:'seu usuario foi criado'});
+ });
 
 module.exports = router;
