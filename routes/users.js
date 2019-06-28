7a6f0f76d6deb8f65b0c420ba72f0b3ee3f44079
Jose Mauro <developer@v8system.com>
2019-06-28 10:47:57 -0300

    bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     User.find({}, (err, data) => {
@@ -19,9 +20,29 @@ router.post('/create', (req, res) => {
 
         User.create(req.body, (err, data) => {
             if (err) return res.send({ error: 'Erro ao criar usuário' });
+            data.password = undefined;
             return res.send(data);
         });
     });
+});
+
+router.post('/auth', (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) return res.send({ error: 'Dados insuficientes!:-(' });
+
+    User.findOne({ email }, (err, data) => {
+
+        if (err) return res.send({ error: 'Erro ao busca usuário' });
+        if (!data) return res.send({ error: 'Usuário não registrado' });
+
+        bcrypt.compare(password, data.password, (err, same) => {
+            if (!same) return res.send({ error: 'Erro ao autenticar usuário' });
+            data.password = undefined;
+            return res.send(data);
+        })
+    }).select('+password');
+
 });
 
 module.exports = router;
